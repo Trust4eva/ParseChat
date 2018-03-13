03ca5cce5587fb6b6a7f66c0094e8d1467e56964
Trustin Harris <trust@Trustins-MacBook-Pro.local>
2018-03-13 19:07:11 -0400

    commit

--- ParseChat/AppDelegate.swift
+++ ParseChat/AppDelegate.swift
@@ -7,14 +7,35 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    
+    
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        Parse.initialize(with: ParseClientConfiguration(block: { (configuration: ParseMutableClientConfiguration) in
+            configuration.applicationId = "CodePath-Parse"
+            configuration.server = "http://45.79.67.127:1337/parse"
+        }))
+        /**
+        if let currentUser = PFUser.current() {
+            print("Welcome back \(currentUser.username!) 😀")
+                
+                // TODO: Load Chat view controller and set as root view controller
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let ChatViewController = storyboard.instantiateViewController(withIdentifier: "ChatViewController")
+            window?.rootViewController = ChatViewController
+        }
+**/
+     
+        
         // Override point for customization after application launch.
         return true
     }
